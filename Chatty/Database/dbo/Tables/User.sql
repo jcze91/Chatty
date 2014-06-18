4e7016dbe3a9a657d1b581a65afea039bf5eca5a
Chahine <chahine.m@hotmail.com>
2014-06-18 23:17:01 +0200

    remove useless project

--- Chatty/Database/dbo/Tables/User.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[User] (
-    [Id]        INT            IDENTITY (1, 1) NOT NULL,
-    [Username]  NVARCHAR (MAX) NOT NULL,
-    [Firstname] NVARCHAR (MAX) NOT NULL,
-    [Lastname]  NVARCHAR (MAX) NOT NULL,
-    [IsEnable]  BIT            NOT NULL,
-    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
