e235b2940fa55a4283778f245aba5382b91760a1
Chahine <chahine.m@hotmail.com>
2014-06-19 10:48:15 +0200

    init signalr

---
+++ Chatty/BackOffice/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(BackOffice.Startup))]
+namespace BackOffice
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
