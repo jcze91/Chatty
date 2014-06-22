c48c874f3f3c420ce9e61fd36889f7bdb80347df
Chahine <chahine.m@hotmail.com>
2014-06-22 16:29:48 +0200

    fix injection

--- Chatty/BackOffice/Configs/ContainerInjection.cs
+++ Chatty/BackOffice/Configs/ContainerInjection.cs
@@ -1,5 +1,7 @@
 ﻿using BackOffice.DataAccess;
+using BackOffice.Providers;
 using BackOffice.Services;
+using BackOffice.Utils;
 using Microsoft.Practices.Unity;
 
 namespace BackOffice.Configs
@@ -13,6 +15,7 @@ namespace BackOffice.Configs
             /**
              * DataAccess
              */
+            this.RegisterInstance<ContactDao>(new ContactDao());
             this.RegisterInstance<DiscussionDao>(new DiscussionDao());
             this.RegisterInstance<GroupDao>(new GroupDao());
             this.RegisterInstance<GroupUserDao>(new GroupUserDao());
@@ -24,6 +27,7 @@ namespace BackOffice.Configs
             /**
              * Services
              */
+            this.RegisterInstance<ContactService>(new ContactService());
             this.RegisterInstance<DiscussionService>(new DiscussionService());
             this.RegisterInstance<GroupService>(new GroupService());
             this.RegisterInstance<GroupUserService>(new GroupUserService());
@@ -31,6 +35,23 @@ namespace BackOffice.Configs
             this.RegisterInstance<MessageService>(new MessageService());
             this.RegisterInstance<DepartmentService>(new DepartmentService());
             this.RegisterInstance<UserService>(new UserService());
+
+            /**
+             * Runtime
+             */
+            this.RegisterInstance<Runtime>(new Runtime());
+            
+            /**
+             * Providers
+             */
+            this.RegisterInstance<ContactProvider>(new ContactProvider());
+            this.RegisterInstance<DiscussionProvider>(new DiscussionProvider());
+            this.RegisterInstance<GroupProvider>(new GroupProvider());
+            this.RegisterInstance<GroupUserProvider>(new GroupUserProvider());
+            this.RegisterInstance<InvitationProvider>(new InvitationProvider());
+            this.RegisterInstance<MessageProvider>(new MessageProvider());
+            this.RegisterInstance<DepartmentProvider>(new DepartmentProvider());
+            this.RegisterInstance<UserProvider>(new UserProvider());
         }
     }
 }
---
+++ Chatty/BackOffice/Providers/MessageProvider.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackOffice.Providers
+{
+    public class MessageProvider : Utils.BaseProvider<int, Dbo.Message, DataAccess.MessageDao, Services.MessageService>
+    {
+        protected override string getClass() { return "message"; }
+
+        protected override int GetFieldCount() { return 4; }
+
+        protected override dynamic Insert(string[] args)
+        {
+            try
+            {
+                return service.Insert(new Dbo.Message()
+                {
+                    UserFromId = int.Parse(args[1]),
+                    UserToId = int.Parse(args[2]),
+                    Content = args[3]
+                });
+            }
+            catch { return null; }
+        }
+
+        protected override dynamic Update(string[] args)
+        {
+            try
+            {
+                return service.Update(new Dbo.Message()
+                {
+                    Id = int.Parse(args[1]),
+                    UserFromId = int.Parse(args[2]),
+                    UserToId = int.Parse(args[3]),
+                    Content = args[4]
+                });
+            }
+            catch { return null; }
+        }
+    }
+}
